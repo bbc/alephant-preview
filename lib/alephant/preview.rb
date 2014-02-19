caae26fdbe7a31b598f050e4f79913b7d1a997c0
Robert Kenny <kenoir@gmail.com>
2014-02-19 08:27:49 +0000

    fix for missing file

--- lib/alephant/preview.rb
+++ lib/alephant/preview.rb
@@ -1,3 +1,6 @@
+lib = File.expand_path('../..', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
 require "alephant/preview/version"
 require "alephant/preview/server"
 require "alephant/preview/template"
---
+++ lib/alephant/preview/template/base.rb
@@ -0,0 +1,47 @@
+require 'mustache'
+
+module Alephant
+  module Preview
+    module Template
+
+      class Base < Mustache
+        attr_accessor :regions
+
+        def initialize(regions, template_location)
+          @regions=regions
+          self.template_file = template_location
+        end
+
+        def static_host
+          ENV['STATIC_HOST'] || 'localhost:8000'
+        end
+
+        def method_missing(name, *args, &block)
+          return super unless respond_to? name.to_s
+          region @regions[name.to_s]
+        end
+
+        def respond_to?(method)
+          valid_regions.include? method.to_s
+        end
+
+        def region(components)
+          if components.kind_of?(Array)
+            components.join
+          else
+            components
+          end
+        end
+
+        def valid_regions
+          self.template.tokens.find_all { |token|
+            token.is_a?(Array) && token[0] == :mustache
+          }.map{ |token|
+            token[2][2][0].to_s
+          }
+        end
+
+      end
+    end
+  end
+end
