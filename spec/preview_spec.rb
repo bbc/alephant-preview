39a7428e7dfd399b32d72171665b0960409f76a0
Robert Kenny <kenoir@gmail.com>
2014-02-24 16:52:25 +0000

    add spec for preview template

--- spec/preview_spec.rb
+++ spec/preview_spec.rb
@@ -7,10 +7,21 @@ describe Alephant::Preview::Server do
     Alephant::Preview::Server
   end
 
+  describe '/preview/:id/:template/:region/?:fixture?' do
+    context 'valid component' do
+      it "should return the rendered component inside the page region specified" do
+        get '/preview/foo/foo/page_region/foo'
+
+        expect(last_response).to be_ok
+        expect(last_response.body).to eq("topcontent\nbottom\n")
+      end
+    end
+  end
+
   describe '/component/:id/:template/?:fixture?' do
     context 'valid component' do
       it "should return the rendered component" do
-        get '/component/foo/foo'
+        get '/component/foo/foo/foo'
 
         expect(last_response).to be_ok
         expect(last_response.body).to eq("content\n")
